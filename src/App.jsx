70912ccf32727306a32d8d6455243bd9c412a05b
Akinde Dipo <akindedipo@Akindes-MacBook-Air.local>
2024-06-29 17:11:55 +0100

    MakeupWebsite
    Makeup

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+function App() {
+  return (
+    <>
+      <ul className="container">
+        <li>Home</li>
+        <li>Portfolio</li>
+        <li>Bookings</li>
+        <li>Contact</li>
+      </ul>
+      <section className="section">
+        <div className="box boxed">
+          <p>CALGARY</p>
+          <p className="makeup">MAKEUP</p>
+          <p>ARTIST</p>
+        </div>
+        <div className="box">2</div>
+        <div className="box">
+          <h1>About Me</h1>
+          <p>
+            Calgary Makeup Artist Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Natus quasi commodi voluptas amet dolores dolorem,
+            rerum pariatur. Unde, neque laudantium reprehenderit id, corporis,
+            cum quaerat nostrum ratione vel quibusdam consequuntur.
+          </p>
+        </div>
+        <div className="box">4</div>
+        <div className="box">5</div>
+        <div className="box">6</div>
+        <div className="box">7</div>
+      </section>
+    </>
+  );
+}
+
+export default App;
