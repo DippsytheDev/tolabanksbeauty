0c4b9d9886bb0d400b714bc78de0c9299bb26a66
Akinde Dipo <akindedipo@Akindes-MacBook-Air.local>
2024-06-30 15:06:15 +0100

    Updated Css
    Update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         <li>Bookings</li>
         <li>Contact</li>
       </ul>
-      <section className="section">
+      <section className="first">
         <div className="box boxed">
           <p>CALGARY</p>
           <p className="makeup">MAKEUP</p>
@@ -23,10 +23,52 @@ function App() {
             cum quaerat nostrum ratione vel quibusdam consequuntur.
           </p>
         </div>
-        <div className="box">4</div>
+        <div className="box">
+          <h1>SERVICES</h1>
+          <h2 className="unicode">&#709;</h2>
+          <ul>
+            <li>Bridal Makeup</li>
+            <li>Group Makeup</li>
+            <li>Events Makeup</li>
+            <li>Theatre Makeup</li>
+          </ul>
+        </div>
         <div className="box">5</div>
         <div className="box">6</div>
-        <div className="box">7</div>
+        <div className="box">
+          <h1>CLIENTS</h1>
+          <h2 className="unicode">&#709;</h2>
+          <ul>
+            <li>Ad Works</li>
+            <li>Ad Works</li>
+            <li>Ad Works</li>
+            <li>Ad Works</li>
+          </ul>
+        </div>
+      </section>
+      <section className="second">
+        <div>
+          <h1>BOOK A SERVICE</h1>
+        </div>
+        <div className="boxed">1</div>
+        <div className="boxed">2</div>
+        <div className="boxed">3</div>
+        <div className="boxed">4</div>
+        <div className="boxed">5</div>
+        <div className="boxed">6</div>
+      </section>
+      <section className="third">
+        <div className="boxer">1</div>
+        <div className="boxer">2</div>
+        <div className="boxer">3</div>
+        <div className="boxer">4</div>
+        <div className="boxer">5</div>
+        <div className="boxer">6</div>
+        <div className="boxer">7</div>
+        <div className="boxer">8</div>
+        <div className="boxer">9</div>
+        <div className="boxer">10</div>
+        <div className="boxer">12</div>
       </section>
     </>
   );
