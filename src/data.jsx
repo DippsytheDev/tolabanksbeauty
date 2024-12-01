a77b87d3a60b019730b28f85aa9b389a66d22365
Akinde Dipo <akindedipo@gmail.com>
2024-12-01 23:26:44 +0100

    Changed Price

--- src/data.jsx
+++ src/data.jsx
@@ -6,7 +6,7 @@ export const people = [
     description:
       "This session includes free consultation and a bridal trial,before your wedding day.",
     Duration: "2hrs",
-    Price: "$450",
+    Price: "$500",
   },
   {
     id: 2,
@@ -15,17 +15,17 @@ export const people = [
     description:
       "This session includes free consultation only before your wedding day. N.B: No bridal trial is included.",
     Duration: "2hrs",
-    Price: "$400",
+    Price: "$450",
   },
   {
-    id: 3,
+    id: 4,
     img: "/Images/img10.jpg",
     name: "NON-BRIDAL SESSION(Home Service)",
     Duration: "1hr 30mins",
     Price: "$200",
   },
   {
-    id: 4,
+    id: 5,
     img: "/Images/img11.jpeg",
     name: "NON-BRIDAL SESSION(In-Studio)",
     Duration: "1hr 30mins",
@@ -33,14 +33,14 @@ export const people = [
     Location: "Livingston, NW",
   },
   {
-    id: 5,
+    id: 6,
     img: "/Images/img1.jpg",
     name: "BRIDAL TRAIN MAKEUP",
     Duration: "1hr 30mins",
     Price: "$200",
   },
   {
-    id: 6,
+    id: 7,
     img: "/Images/img14.jpg",
     name: "DIY MAKEUP CLASS",
     Duration: "3hrs",
