f68aadf3a1630e0adfb3a8ddb469f2e7ac5a5719
Akinde Dipo <akindedipo@gmail.com>
2025-07-17 23:08:24 +0100

    More booking functionality

--- src/Components/BookCard/Booking.jsx
+++ src/Components/BookCard/Booking.jsx
@@ -170,16 +170,49 @@ const Booking = ({ isOpen, onRequestClose, service }) => {
       moment(date).isSameOrAfter(septFirst2025) &&
       moment(date).isBefore(janFirst2026)
     ) {
-      // Only 16:00, 16:30, 17:00, 17:30, 18:00, 18:30, 19:00
-      allTimes = [
-        "16:00",
-        "16:30",
-        "17:00",
-        "17:30",
-        "18:00",
-        "18:30",
-        "19:00",
-      ];
+      const dayOfWeek = moment(date).day();
+      if (dayOfWeek === 0 || dayOfWeek === 6) {
+        // Weekend: all times available
+        allTimes = [
+          "06:30",
+          "07:00",
+          "07:30",
+          "08:00",
+          "08:30",
+          "09:00",
+          "09:30",
+          "10:00",
+          "10:30",
+          "11:00",
+          "11:30",
+          "12:00",
+          "12:30",
+          "13:00",
+          "13:30",
+          "14:00",
+          "14:30",
+          "15:00",
+          "15:30",
+          "16:00",
+          "16:30",
+          "17:00",
+          "17:30",
+          "18:00",
+          "18:30",
+          "19:00",
+        ];
+      } else {
+        // Weekday: only 16:00–19:00
+        allTimes = [
+          "16:00",
+          "16:30",
+          "17:00",
+          "17:30",
+          "18:00",
+          "18:30",
+          "19:00",
+        ];
+      }
     }
     // Before September 1, 2025: only weekends allowed
     if (moment(date).isBefore(septFirst2025)) {
