b94e034aac72468ce869168a1e990a91d9ca9d13
Akinde Dipo <akindedipo@gmail.com>
2026-01-12 02:08:10 +0100

    adjustment

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -36,7 +36,7 @@ const About = () => {
         </div>
 
         <div className="about-image-right">
-          <img src="/Images/img21.jpg" alt="Bims - Makeup Artist" />
+          <img src="/Images/img46.jpg" alt="Bims - Makeup Artist" />
           <div className="watermark-text watermark-right">MAKEUP ARTIST</div>
         </div>
       </div>
